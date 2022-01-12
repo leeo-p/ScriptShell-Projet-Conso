64f5f037714f11991b3b7b6545d2711ac5854f29
Leo Portet <leo.portet@eisti.fr>
2022-01-12 09:14:55 +0100

    Update monScript.sh

--- monScript.sh
+++ monScript.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 
+echo "Le paramètre est $1"
+
+pays=$1
+echo $pays
+
+i=0
+
+while [ true ]
+do
+	colonne=head -1 Sources/Country_Consumption_TWH.csv | cut -d',' -f$i 
+	if [ $pays=$colonne ];then
+		cut -d',' -f$i Sources/Country_Consumption_TWH.csv > conso.csv
+		break
+	else
+		$i=$i+1
+	fi
+done
+
+if [ -d Resultats ];then
+	echo "Le dossier Resultat existe déjà"
+	if [ -d $pays ];then
+		echo "Le dossier $pays existe déjà !"
+	else 
+		mkdir Resultats/$pays
+	fi
+else 
+	mkdir Resultats
+	if [ -d $pays ];then
+		echo "Le dossier existe déjà !"
+	else 
+		mkdir Resultats/$pays
+	fi
+fi
+
 
