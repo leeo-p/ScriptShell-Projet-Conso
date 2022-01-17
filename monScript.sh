11c10df4624c0462ee2089bbe73f94f7babcd1ad
Leo Portet <leo.portet@eisti.fr>
2022-01-17 12:26:19 +0100

    Update monScript.sh

--- monScript.sh
+++ monScript.sh
@@ -5,33 +5,29 @@ echo "Le paramètre est $1"
 pays=$1
 echo $pays
 
-i=0
+i=1
 
-while [ true ]
+while [[ `head -1 Sources/Country_Consumption_TWH.csv | cut -d',' -f"$i"` ]]
 do
-	colonne=`head -1 Sources/Country_Consumption_TWH.csv | cut -d',' -f$i` 
-	if [ $pays=$colonne ];then
-		cut -d',' -f$i Sources/Country_Consumption_TWH.csv > conso.csv
+	if [[ "$pays" = `head -1 Sources/Country_Consumption_TWH.csv | cut -d',' -f"$i"` ]];then
+		if [ -d Resultats ];then
+			echo "Le dossier Resultat existe déjà"
+			if [ -d "$pays" ];then
+				echo "Le dossier "$pays" existe déjà !"
+			else
+				mkdir Resultats/"$pays"
+			fi
+		else
+			mkdir Resultats
+			if [ -d "$pays" ];then
+				echo "Le dossier existe déjà !"
+			else
+				mkdir Resultats/"$pays"
+			fi
+		fi
+		cut -d',' -f"$i" Sources/Country_Consumption_TWH.csv > Resultats/"$pays"/conso.csv
 		break
 	else
-		$i=$i+1
+		let "i++"
 	fi
 done
-
-if [ -d Resultats ];then
-	echo "Le dossier Resultat existe déjà"
-	if [ -d $pays ];then
-		echo "Le dossier $pays existe déjà !"
-	else 
-		mkdir Resultats/$pays
-	fi
-else 
-	mkdir Resultats
-	if [ -d $pays ];then
-		echo "Le dossier existe déjà !"
-	else 
-		mkdir Resultats/$pays
-	fi
-fi
-
-
