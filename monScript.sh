02149854c399e70e8e1e3959d4d9a6cf19f6129f
Leo Portet <leo.portet@eisti.fr>
2022-01-18 13:35:05 +0100

    Update monScript.sh

--- monScript.sh
+++ monScript.sh
@@ -1,33 +1,69 @@
 #!/bin/bash
 
-echo "Le paramètre est $1"
+echo "Le paramètre 1 est $1"
+echo "Le paramètre 2 est $2"
 
-pays=$1
-echo $pays
+type=$1
+nom=$2
+echo $nom
+echo $type
+
+country="Country"
+continent="Continent"
 
 i=1
 
-while [[ `head -1 Sources/Country_Consumption_TWH.csv | cut -d',' -f"$i"` ]]
-do
-	if [[ "$pays" = `head -1 Sources/Country_Consumption_TWH.csv | cut -d',' -f"$i"` ]];then
-		if [ -d Resultats ];then
-			echo "Le dossier Resultat existe déjà"
-			if [ -d "$pays" ];then
-				echo "Le dossier "$pays" existe déjà !"
+if [[ "$type" = "$country" ]];then 
+	while [[ `head -1 Sources/Country_Consumption_TWH.csv | cut -d',' -f"$i"` ]]
+	do
+		if [[ "$nom" = `head -1 Sources/Country_Consumption_TWH.csv | cut -d',' -f"$i"` ]];then
+			if [ -d Resultats ];then
+				echo "Le dossier Resultat existe déjà"
+				if [ -d "$nom" ];then
+					echo "Le dossier "$nom" existe déjà !"
+				else
+					mkdir Resultats/"$nom"
+				fi
 			else
-				mkdir Resultats/"$pays"
+				mkdir Resultats
+				if [ -d "$nom" ];then
+					echo "Le dossier existe déjà !"
+				else
+					mkdir -p Resultats/"$nom"
+				fi
 			fi
+			cut -d',' -f"$i" Sources/Country_Consumption_TWH.csv > Resultats/"$nom"/conso.csv
+			break
 		else
-			mkdir Resultats
-			if [ -d "$pays" ];then
-				echo "Le dossier existe déjà !"
-			else
-				mkdir Resultats/"$pays"
-			fi
+			let "i++"
 		fi
-		cut -d',' -f"$i" Sources/Country_Consumption_TWH.csv > Resultats/"$pays"/conso.csv
-		break
-	else
-		let "i++"
-	fi
-done
+	done
+elif [[ "$type"="$continent" ]];then
+	while [[ `head -1 Sources/Continent_Consumption_TWH.csv | cut -d',' -f"$i"` ]]
+	do 
+		if [[ "$nom" = `head -1 Sources/Continent_Consumption_TWH.csv | cut -d',' -f"$i"` ]];then
+			if [ -d Resultats ];then
+				echo "Le dossier Resultat existe déjà"
+				if [ -d "$nom" ];then
+					echo "Le dossier "$nom" existe déjà !"
+				else
+					mkdir Resultats/"$nom"
+				fi
+			else
+				mkdir Resultats
+				if [ -d "$nom" ];then
+					echo "Le dossier existe déjà !"
+				else
+					mkdir -p Resultats/"$nom"
+				fi
+			fi
+			cut -d',' -f"$i" Sources/Continent_Consumption_TWH.csv > Resultats/"$nom"/conso.csv
+			break
+		else
+			let "i++"
+		fi
+	done
+else 
+	echo "Recommence t'a écris de la merde frérot !"
+fi
+		
