3e0c051871f8a8a43ab91b8de1b27b4ea0252dd5
Leo Portet <leo.portet@eisti.fr>
2022-01-18 17:03:59 +0100

    Update monScript.sh

--- monScript.sh
+++ monScript.sh
@@ -5,20 +5,21 @@ echo "Le paramètre 2 est $2"
 
 type=$1
 nom=$2
-echo $nom
+
 echo $type
+echo $nom
 
 country="Country"
 continent="Continent"
 
 i=1
 
-if [[ "$type" = "$country" ]];then 
+if [[ "$type" = "$country" ]];then
 	while [[ `head -1 Sources/Country_Consumption_TWH.csv | cut -d',' -f"$i"` ]]
 	do
 		if [[ "$nom" = `head -1 Sources/Country_Consumption_TWH.csv | cut -d',' -f"$i"` ]];then
 			if [ -d Resultats ];then
-				echo "Le dossier Resultat existe déjà"
+				echo "Le dossier Resultats existe déjà"
 				if [ -d "$nom" ];then
 					echo "Le dossier "$nom" existe déjà !"
 				else
@@ -32,18 +33,22 @@ if [[ "$type" = "$country" ]];then
 					mkdir -p Resultats/"$nom"
 				fi
 			fi
-			cut -d',' -f"$i" Sources/Country_Consumption_TWH.csv > Resultats/"$nom"/conso.csv
+			cut -d',' -f1 Sources/Country_Consumption_TWH.csv > Resultats/"$nom"/conso.csv
+			cut -d',' -f"$i" Sources/Country_Consumption_TWH.csv >> Resultats/"$nom"/conso.csv
 			break
 		else
 			let "i++"
 		fi
 	done
-elif [[ "$type"="$continent" ]];then
+else
+	echo "Recommence t'as écris de la merde frérot !"
+fi
+if [[ "$type"="$continent" ]];then
 	while [[ `head -1 Sources/Continent_Consumption_TWH.csv | cut -d',' -f"$i"` ]]
-	do 
+	do
 		if [[ "$nom" = `head -1 Sources/Continent_Consumption_TWH.csv | cut -d',' -f"$i"` ]];then
 			if [ -d Resultats ];then
-				echo "Le dossier Resultat existe déjà"
+				echo "Le dossier Resultats existe déjà"
 				if [ -d "$nom" ];then
 					echo "Le dossier "$nom" existe déjà !"
 				else
@@ -57,13 +62,13 @@ elif [[ "$type"="$continent" ]];then
 					mkdir -p Resultats/"$nom"
 				fi
 			fi
-			cut -d',' -f"$i" Sources/Continent_Consumption_TWH.csv > Resultats/"$nom"/conso.csv
+			cut -d',' -f1 Sources/Continent_Consumption_TWH.csv > Resultats/"$nom"/conso.csv
+			cut -d',' -f"$i" Sources/Continent_Consumption_TWH.csv >> Resultats/"$nom"/conso.csv
 			break
 		else
 			let "i++"
 		fi
 	done
-else 
-	echo "Recommence t'a écris de la merde frérot !"
+else
+	echo "Recommence t'as écris de la merde frérot !"
 fi
-		
