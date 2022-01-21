024baa14c0a6674583326a499e109b58c92433d9
Leo Portet <leo.portet@eisti.fr>
2022-01-21 11:01:47 +0100

    Update monScript.sh

--- monScript.sh
+++ monScript.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-echo "Le paramètre 1 est $1"
-echo "Le paramètre 2 est $2"
+#-------------------------------------------------------Projet de ScriptShell-------------------------------------------------------#
+#-----Réalisé par Laura Sabadie & Léo Portet----------------------------------------------------------------------------------------#
+#-----Réalisé du 10/01/21 au 21/01/21-----------------------------------------------------------------------------------------------#
+#-----Ce script vous permet de réaliser un traitement de données, de les répertorier et d'en faire des graphiques-------------------#
+#-----------------------------------------------------------------------------------------------------------------------------------#
 
-type=$1 #Variable prenant le premier paramètre qui définit si le deuxième est un continant ou un pays
-nom=$2 #Variable prenant le deuxième paramètre qui indique le nom du pays ou du continent
+type=$1 #variable prenant le premier paramètre qui définit si le deuxième est un continant ou un pays
+nom=$2 #variable prenant le deuxième paramètre qui indique le nom du pays ou du continent
+
+echo $type
+echo $nom
 
 country="Country"
 continent="Continent"
 
-i=1
+i=1 #variable d'incrémentation
 
 #Fonction permettant de faire un graph à partir de données récupérées dans les fichiers sources
 function graph() {
-	gnuplot -e "reset; 
+	gnuplot -e "reset;
 	set terminal png;
 	set output 'Resultats/$1/conso.png';
 	set datafile separator ',';
@@ -24,6 +30,7 @@ function graph() {
 	"
 }
 
+#Fonction permettant de faire un graph de comparaison des différentes productions d'énergies renouvelables
 function comparegraph() {
 	gnuplot -e "reset; 
 	set terminal png;
@@ -32,10 +39,9 @@ function comparegraph() {
 	set xlabel 'Productions';
 	set ylabel 'Energies';
 	set title 'Conso par an  de : $1';
-	plot 'Resultats/$1/compare.csv' u 1:2 w l title '$1'; 
+	plot 'Resultats/$1/compare.csv' u 1:2 'compare.csv' u 1:3 'compare.csv' u 1:4 'compare.csv' u 1:5 w l title '$1'; 
 	"
 }
-#'Resultats/$1/compare.csv' u 1:3 w l title '$1','Resultats/$1/compare.csv' u 1:4 w l title '$1','Resultats/$1/compare.csv' u 1:2 w l title '$1';
 
 #Fonction permettant de grapher un histogramme
 function graphisto() {
@@ -53,63 +59,65 @@ function graphisto() {
 	set xtics nomirror rotate by -45 scale 0;
 	plot 'Resultats/$1/prod.csv' u 2:xtic(1) lt 2 notitle;
 	"
-	#lt 2 sert à mettre la couleur verte
-	#xtic(1) sert à mettre la colonne 1 en intitulés d'abscisse
+	#lt 2 sert a mettre la couleur verte
+	# xtic(1) sert à mettre la colonne 1 en intitulés d'abscisse
+	#xtics nomirror rotate by -45 sert à incliner de 45 degrés les noms de pays pour qu'ils soient plus lisibles
 }
 
-if [[ "$type" = "$country" ]];then #Si le paramètre 1 = Country
+if [[ "$type" = "$country" ]];then #si le paramètre 1 = Country
 	while [[ `head -1 Sources/Country_Consumption_TWH.csv | cut -d',' -f"$i"` ]] #on parcours ce fichier colonne par colonne
 	do
-		if [[ "$nom" = `head -1 Sources/Country_Consumption_TWH.csv | cut -d',' -f"$i"` ]];then #Si paramètre 2 = nom du pays cherché
-			if [ -d Resultats ];then #On vérifie si le dossier existe
+		if [[ "$nom" = `head -1 Sources/Country_Consumption_TWH.csv | cut -d',' -f"$i"` ]];then #si le paramètre 2 = nom du pays cherché
+			if [ -d Resultats ];then #on vérifie si le dossier existe
 				echo "Le dossier Resultats existe déjà"
-				cd Resultats #Si oui on rentre dedans
-				if [ -d "$nom" ];then #On vérifie si le dossier du pays existe
-					echo "Le dossier "$nom" existe déjà !" #Si oui on ne fait rien
+				cd Resultats #si oui on rentre dedans
+				if [ -d "$nom" ];then #on vérifie si le dossier du pays existe
+					echo "Le dossier "$nom" existe déjà !" #si oui on ne fait rien
 				else
-					mkdir "$nom" #Sinon on le créer
+					mkdir "$nom" #sinon on le crée
 				fi
-				cd .. #On sort du dossier Resultats
+				cd .. #on sort du dossier Resultats
 			else
-				mkdir -p Resultats/"$nom" #Sinon on créer récursivement les deux dossiers
+				mkdir -p Resultats/"$nom" #sinon on crée récursivement les deux dossiers
 			fi
-			cut -d',' -f1,"$i" Sources/Country_Consumption_TWH.csv > Resultats/"$nom"/conso.csv #On crée le fichier de sortie et on y met les informations utiles
-			graph $nom #Apel de la fonction graph 
-			break #On sort de la boucle while
+			cut -d',' -f1,"$i" Sources/Country_Consumption_TWH.csv > Resultats/"$nom"/conso.csv #on crée le fichier de sortie avec les infos du pays
+			graph $nom #appel de la fonction graph
+			break #on sort de la boucle
 		else
-			let "i++" #On incrémente i
+			let "i++" #on incrémente i
 		fi
 	done
-elif [[ "$type" = "$continent" ]];then # si param 1 = Continent
-	while [[ `head -1 Sources/Continent_Consumption_TWH.csv | cut -d',' -f"$i"` ]]
+elif [[ "$type" = "$continent" ]];then #si le paramètre 1 = Continent
+	while [[ `head -1 Sources/Continent_Consumption_TWH.csv | cut -d',' -f"$i"` ]] #on parcours le fichier colonne par colonne
 	do
-		if [[ "$nom" = `head -1 Sources/Continent_Consumption_TWH.csv | cut -d',' -f"$i"` ]];then
-			if [ -d Resultats ];then
+		if [[ "$nom" = `head -1 Sources/Continent_Consumption_TWH.csv | cut -d',' -f"$i"` ]];then #si le paramètre 2 = nom du continent cherché 
+			if [ -d Resultats ];then #on vérifie si le dossier existe
 				echo "Le dossier Resultats existe déjà"
-				cd Resultats
-				if [ -d "$nom" ];then
-					echo "Le dossier "$nom" existe déjà !"
+				cd Resultats #si oui on rentre dedans
+				if [ -d "$nom" ];then #on vérifie si le dossier du continent existe
+					echo "Le dossier "$nom" existe déjà !" #si oui on ne fait rien
 				else
-					mkdir "$nom"
+					mkdir "$nom" #sinon on le crée
 				fi
-				cd ..
+				cd .. #on sort du dossier Resultats
 			else
-				mkdir -p Resultats/"$nom"
+				mkdir -p Resultats/"$nom" #sinon on crée récursivement le deux dossiers
 			fi
-			cut -d',' -f1,"$i" Sources/Continent_Consumption_TWH.csv > Resultats/"$nom"/conso.csv
-			graph $nom
-			break
+			cut -d',' -f1,"$i" Sources/Continent_Consumption_TWH.csv > Resultats/"$nom"/conso.csv #on crée le fichier de sotie avec les infos du continent
+			graph $nom #appel de la fonction graph
+			break #on sort de la boucle
 		else
-			let "i++"
+			let "i++" #on incrémente i
 		fi
 	done
-elif [[ "$type" = "Renouv_par_pays" ]];then # si param 1 = Renouv_par_pays
+elif [[ "$type" = "Renouv_par_pays" ]];then #si le paramètre 1 = Renouv_par_pays
 	nom_dossier="Renouvelables"
+	#vérification de l'existence des dossiers
 	if [ -d Resultats ];then
 		echo "Le dossier Resultats existe déjà"
 		cd Resultats
 		if [ -d "$nom_dossier" ];then
-			echo "Le dossier '$nom_dossier' existe déjà !"
+			echo "Le dossier $nom_dossier existe déjà !"
 		else
 			mkdir $nom_dossier
 		fi
@@ -117,15 +125,17 @@ elif [[ "$type" = "Renouv_par_pays" ]];then # si param 1 = Renouv_par_pays
 	else
 		mkdir -p Resultats/$nom_dossier
 	fi
-	cut -d',' -f1,6 Sources/top20CountriesPowerGeneration.csv > Resultats/"$nom_dossier"/prod.csv
-	graphisto $nom_dossier
-elif [[ "$type" = "Energie_Compare" ]];then
+	cut -d',' -f1,6 Sources/top20CountriesPowerGeneration.csv > Resultats/"$nom_dossier"/prod.csv #on génère le fichier de sortie avec les infos attendues pour le graph
+	graphisto $nom_dossier #appel de la fonction graphisto
+
+elif [[ "$type" = "Energie_Compare" ]];then #si le paramètre 1 = Energie_Compare
 	nom_dossier="Comparaison"
+	#vérification de l'existance des dossiers
 	if [ -d Resultats ];then
 		echo "Le dossier Resultats existe déjà"
 		cd Resultats
 		if [ -d "$nom_dossier" ];then
-			echo "Le dossier '$nom_dossier' existe déjà !"
+			echo "Le dossier $nom_dossier existe déjà !"
 		else
 			mkdir $nom_dossier
 		fi
@@ -133,19 +143,18 @@ elif [[ "$type" = "Energie_Compare" ]];then
 	else
 		mkdir -p Resultats/$nom_dossier
 	fi
-	cut -d',' -f1-5 Sources/top20CountriesPowerGeneration.csv > Resultats/"$nom_dossier"/compare.csv
-	comparegraph $nom_dossier
+	cut -d',' -f1-5 Sources/top20CountriesPowerGeneration.csv > Resultats/"$nom_dossier"/compare.csv #on génère le fichier de sortie avec les infos attendues pour le graph
+	comparegraph $nom_dossier #appel de la fonction comparegraph
 
-elif [[ "$type" = "Renouvelable" ]];then
-	pays=`sort -t',' -k6nr Sources/top20CountriesPowerGeneration.csv | head -1 | cut -d',' -f1`
-	res=`sort -t',' -k6nr Sources/top20CountriesPowerGeneration.csv | head -1 | cut -d',' -f6`
-	echo "$pays : $res TWh"
-elif [[ "$type" = "Non_Renouvelable" ]];then
-	pays=`sort -t',' -k6n Sources/top20CountriesPowerGeneration.csv | head -2 | tail -1 | cut -d',' -f1`
-	res=`sort -t',' -k6n Sources/top20CountriesPowerGeneration.csv | head -2 | tail -1 | cut -d',' -f6`
-	echo "$pays : $res TWh"
+#Affichages terminal
+elif [[ "$type" = "Renouvelable" ]];then #si le paramètre 1 = Renouvelable
+	pays=`sort -t',' -k6nr Sources/top20CountriesPowerGeneration.csv | head -1 | cut -d',' -f1` #on trie le fichier pour trouver le pays
+	res=`sort -t',' -k6nr Sources/top20CountriesPowerGeneration.csv | head -1 | cut -d',' -f6` #on trie le fichier pour trouver sa production
+	echo "Le pays qui produit le plus est : $pays, en effet il produit $res TWh" #on affiche dans le terminal les éléments recherchés
+elif [[ "$type" = "Non_Renouvelable" ]];then #si le paramètre 1 = Non_Renouvelable
+	pays=`sort -t',' -k6n Sources/top20CountriesPowerGeneration.csv | head -2 | tail -1 | cut -d',' -f1` #on trie le fichier pour trouver le pays
+	res=`sort -t',' -k6n Sources/top20CountriesPowerGeneration.csv | head -2 | tail -1 | cut -d',' -f6` #on trie le fichier pour trouver sa production
+	echo "Le pays qui produit le moins est : $pays, en effet il produit $res TWh" #on affiche dans le terminal les éléments recherchés
 else
-	echo "Recommence t'as écris de la merde frérot !"
+	echo "Veuillez recommencer le paramètre n°1 n'est pas valide, merci."
 fi
-
-
